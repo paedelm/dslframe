a7c8099c7ae8df7bf0f87f6c8a127af77b3fcfe2
Peter Edelman <peter.edelman@nn.nl>
2019-07-02 17:41:02 +0200

    json processing

--- Program.fs
+++ Program.fs
@@ -5,6 +5,8 @@ open Ftest
 open proc
 open Web
 open files
+open FSharp.Data
+open FSharp.Data.JsonExtensions
 
 [<EntryPoint>]
 let main argv =
@@ -15,14 +17,32 @@ let main argv =
         for line in fd1 do printfn "fd1:%s" line
         for line in fd2 do printfn "fd2:%s" line
         } |> Async.RunSynchronously
-    try 
-        // let result = Async.RunSynchronously(downLoadUrl("http://google.com"))
-        // printfn "%A" result
-        let res2 = Async.RunSynchronously(downLoadUrl("http://api.worldbank.org/v2/region?format=json"))
-        printfn "%A" res2
-    with
-    | exdl -> 
-        printfn "%A" (exdl.GetBaseException())
+    // let result = Async.RunSynchronously(downLoadUrl("http://google.com"))
+    // printfn "%A" result
+    async {
+        let! resj = downLoadUrl("http://api.worldbank.org/v2/region?format=json")
+        let jv = JsonValue.Parse(resj)
+        // printfn "%A" jv
+        match jv with
+        | JsonValue.Array [| info; data |] ->
+            // Print overall information
+            let page, pages, total = info?page, info?pages, info?total
+            printfn 
+              "Showing page %d of %d. Total records %d" 
+              (page.AsInteger()) (pages.AsInteger()) (total.AsInteger())
+            printfn "%A" data
+            // Print every non-null data point
+            for record in JsonExtensions.AsArray(data) do
+              if record?name <> JsonValue.Null then
+                printfn "%s: %s" (record?code.AsString()) 
+                                 (record?name.AsString())
+        | _ -> printfn "failed"        
+    } |> Async.RunSynchronously
+    async {
+        let! resx = downLoadUrl("http://api.worldbank.org/v2/region?format=xml")
+        printfn "%A" resx
+    } |> Async.RunSynchronously
+    // Async.Parallel [| dnl1; dnl2 |] |> Async.RunSynchronously
 
     // for line in outp do printfn "%s" line
     try
